d6c7951213430661b3c378f98a472eb6ed921b79
Joffreyjames <152128394+joffreyjames@users.noreply.github.com>
2024-05-30 18:30:05 -0700

    modified:   falling.py

--- falling.py
+++ falling.py
@@ -14,31 +14,63 @@ WHITE = (255, 255, 255)
 SAND = (194, 178, 128)
 T = 8
 
-class Grid:
-    def __init__(self):
-        self.grid = np.zeros((WIDTH*2,HEIGHT+T))
-        self.position = []
+class Grid: # defines "Grid" class
+    def __init__(self): # defines constructor method for "Grid" class
+        self.grid = np.zeros((WIDTH*2,HEIGHT+T)) # initializes instance variable "grid" as NumPy array of zeroes
+        self.position = [] # initializes instance variable "position" as empty list
         
-    def addSand(self, pointX, pointY):
-        if pointX >= 0 and pointX <= WIDTH and pointY >= 0 and pointY <= HEIGHT:
-            if self.grid[pointX][pointY] == 0:
-                self.grid[pointX][pointY] = 1
-                self.position.append((pointX, pointY))
+    def addSand(self, pointX, pointY): # defines method "addSand" that takes three parameters: self, pointX, and pointY
+        if pointX >= 0 and pointX <= WIDTH and pointY >= 0 and pointY <= HEIGHT: # checks for valid coordinates
+            if self.grid[pointX][pointY] == 0: # checks if grid cell is emply
+                self.grid[pointX][pointY] = 1 # adds sand particle
+                self.position.append((pointX, pointY)) #records coordinates to "position" list
     
-    def update_position(self):
-        for points in self.position:
-            listpoints = list(points)
-            self.position.remove(points)
+    def update_position(self): # defines "update_position" method to to update positions of sand particles in self.position
+        for points in self.position: # iterates over all positions in self.position
+            listpoints = list(points) # converts tuple to a list
+            self.position.remove(points) # current position is removes to update its new position later
             
-            if points[1] >= HEIGHT-T:
+            if points[1] >= HEIGHT - T: # if y coordinate of point is >= HEIGHT - T (at bottom of grid), point is re-added to self.position
                 self.position.append(points)
                 
-            elif self.grid[points[0]][points[1]+T] == 0:
-                self.grid[points[0]][points[1]] = 0
-                self.grid[points[0]][points[1]+T] = 1 
-                listpoints[1] += T
-                points = tuple(listpoints)
-                self.position.append(points)
+            elif self.grid[points[0]][points[1]+T] == 0: # checks if cell blow is empty
+                self.grid[points[0]][points[1]] = 0 # current cell is set to 0
+                self.grid[points[0]][points[1]+T] = 1 # cell below is set to 1
+                listpoints[1] += T # moves the y-coordinate of the particle down T units
+                points = tuple(listpoints) # converts list back to a tuple
+                self.position.append(points) # updated position tuple "points" is appended to self.position
+            
+            elif self.grid[points[0]][points[1]+T] == 1: # checks if cell below is occupied
+                if (self.grid[points[0]+T][points[1]+T] == 1) and (self.grid[points[0]-T][points[1]+T] == 1): # checks if both bottom-right and bottom-left are occupied
+                    self.position.append(points) # particle remains at current position
+                    
+                elif (self.grid[points[0]+T][points[1]+T] == 1) and (self.grid[points[0]-T][points[1]+T] == 0): # checks if bottom-right is occupied and bottom-left is empty
+                    self.grid[points[0]][points[1]] = 0 # current cell is set to 0
+                    self.grid[points[0]-T][points[1]+T] = 1 # the particle is moved down and left
+                    listpoints[0] -= T # moves the x-coordinate of the particle left T units
+                    listpoints[1] += T # moves the y-coordinate of the particle down T units
+                    points = tuple(listpoints) # converts list back to a tuple
+                    self.position.append(points) # updated position tuple "points" is appended to self.position
+                
+                elif (self.grid[points[0]+T][points[1]+T] == 0) and (self.grid[points[0]-T][points[1]+T] == 1): # checks if bottom-left is occupied and bottom-right is empty
+                    self.grid[points[0]][points[1]] = 0 # current cell is set to 0
+                    self.grid[points[0]+T][points[1]+T] = 1 # the particle is moved down and right 
+                    listpoints[0] += T # moves the x-coordinate of the particle left T units
+                    listpoints[1] += T # moves the y-coordinate of the particle down T units
+                    points = tuple(listpoints) # converst list back to a tuple
+                    self.position.append(points) # updated position tuple "points" is appended to self.position
+
+                else:
+                    self.grid[points[0]][points[1]] = 0 # checks if  ottom-left and bottom-right cells are both empty
+                    a = random.randint(0,1) # generate random 0 or 1
+                    if a == 0: # if a is 0, changes a to -1
+                        a = -1
+                    self.grid[points[0]+a*T][points[1] + T] = 1 # changes particle to bottom-left or -right as a result of random 0 or 1
+                    listpoints[0] += a*T # moves the x-coordiate of the particle left or right T units
+                    listpoints[1] += T # moves the y-coordinate of the particle down T units
+                    points = tuple(listpoints)
+                    self.position.append(points)
+
                 
     def draw(self, screen):
         for points in self.position:
